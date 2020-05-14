2b5b3961f507f957aa9e9cf016d0382ed84426ff
glorytiger <chrnor3@gmail.com>
2020-05-14 17:04:18 +0200

    Made a simple overlay window

--- js/global.js
+++ js/global.js
@@ -79,6 +79,24 @@ function printUserId() {
   alert(s.substring(1, s.length));
 }
 
+function showNewBoardDiv() {
+  var overlayDiv   = document.getElementById("new-board-overlay");
+  var containerDiv = document.getElementById("new-board-container");
+  overlayDiv.style.display = "block";
+  containerDiv.style.display = "block";
+}
+
+function hideNewBoardDiv() {
+  var overlayDiv   = document.getElementById("new-board-overlay");
+  var containerDiv = document.getElementById("new-board-container");
+  overlayDiv.style.display = "none";
+  containerDiv.style.display = "none";
+}
+
+function createNewBoard() {
+  alert("Work in progress!");
+}
+
 function getUniqueListId(listName) {
 
     const list = JSON.parse(window.localStorage.getItem(listName));
