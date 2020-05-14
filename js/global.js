908f3ed75b682e5c415f677581e7a2fabe62db91
glorytiger <chrnor3@gmail.com>
2020-05-14 15:57:50 +0200

    Login form now pass user id in url when redirecting

--- js/global.js
+++ js/global.js
@@ -2,19 +2,25 @@ function loginUser(event) {
   event.preventDefault();
 
   const loginType = document.activeElement.name;
-  console.log(loginType + "button pressed");
+  //console.log(loginType + " button pressed");
 
-  const name = document.querySelector("[name='username-in']").value;
+  const name = document.querySelector("[name='username-in']").value.toLowerCase();
   const password = document.querySelector("[name='password-in']").value;
 
-  let passMasked = '';
-  for (let i = 0; i < password.length; i++) {passMasked += "*";}
-  console.log("username: " + name + ", password: " + passMasked);
+  if (name.length < 1) {
+      console.log("Invalid username, must be at least 1 characters.");
+      return;
+  }
+  if (password.length < 1) {
+    console.log("Invalid password, must be at least 1 characters.");
+    return;
+  }
+
+  //let passMasked = '';
+  //for (let i = 0; i < password.length; i++) {passMasked += "*";}
+  //console.log("username: " + name + ", password: " + passMasked);
 
   const userList = JSON.parse(window.localStorage.getItem("userList")) || [];
-  if (userList.length == 0) {
-      console.log("no users found in local storage");
-  }
 
   if (loginType == "login") {
        
@@ -22,30 +28,74 @@ function loginUser(event) {
     for (let i = 0; i < userList.length; i++) {
       if (userList[i].name == name) {
 
-      // User found, check password.
-      if (userList[i].password == password) {
-        console.log("user authentication successful");
-          window.location.href = "boards.html";
+        // User found, check password.
+        if (userList[i].password == password) {
+          console.log("user authentication successful");
+          
+          // Check if user has a valid lastBoardId value set.
+          const urlParam = "?" + encodeURIComponent(userList[i].id);
+          if (userList[i].lastBoardId > 0) {
+            window.location.href = "index.html" + urlParam;
+          } else {
+            window.location.href = "boards.html" + urlParam;
+          }
           return;
         } else {
-          console.log("user authentication failed: invalid password");
+          console.log("User authentication failed: invalid password");
           // TODO: output error message in feedback div
         }
       
       }
     }
-    console.log("couldn't find user " + name + " in storage");
+    console.log("Couldn't find user " + name + " in storage");
     // TODO: output error message in feedback div
 
   } else {
 
-      // Add user to storage
-      const id = 0;
-      const user = {id, name, password};
-      userList.push(user);
-      window.localStorage.setItem("userList", JSON.stringify(userList));
+    // Add user to storage
 
-      console.log("user added to storage");
-      // TODO: output feedback message
+    // First check that the user doesn't already exists.
+    for (let i = 0; i < userList.length; i++) {
+      if (userList[i].name == name) {
+          console.log("User " + name + " already in storage");
+          // TODO: output feedback message
+          return;
+      }
+    }
+
+    const id = getUniqueListId(userList);
+    const lastBoardId = 0;
+    const user = {id, name, password, lastBoardId};
+    userList.push(user);
+    window.localStorage.setItem("userList", JSON.stringify(userList));
+
+    console.log("User " + name + " added to storage with id " + id);
+    // TODO: output feedback message
   }
+}
+
+function printUserId() {
+  var s = window.location.search;
+  alert(s.substring(1, s.length));
+}
+
+function getUniqueListId(listName) {
+
+    const list = JSON.parse(window.localStorage.getItem(listName));
+    
+    if (list == undefined) {
+        return 1;
+    }
+    
+    let usedIds = [];
+    for (const entry of list) {
+        console.log("usedIds["+entry.id+"] = true");
+        usedIds[entry.id] = true;
+    }
+    
+    let id = 1;
+    do {
+    } while (usedIds[++id] == true);
+
+    return id;
 }
