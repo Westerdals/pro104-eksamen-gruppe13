20f1aa40022082226c2a9b7695dbbc78f411602c
glorytiger <chrnor3@gmail.com>
2020-05-15 16:00:40 +0200

    New boards are now properly created and stored in local storage

--- js/boards.js
+++ js/boards.js
@@ -12,6 +12,52 @@ function hideNewBoardDiv() {
   containerDiv.style.display = "none";
 }
 
-function createNewBoard() {
-  alert("Work in progress!");
+function createNewBoard(inputIdTag) {
+  const userId = getUserId();
+  const title = document.getElementById(inputIdTag).value;
+
+  if (title.length < 1) {
+    alert("Title must contain at least one character.");
+  } else if (!isAlphaNumeric(title)) {
+    alert("Title can only contain letters and numbers.");
+  }
+
+  // Get board and user list from local storage
+  const userList = JSON.parse(window.localStorage.getItem("userList")) || [];
+  const boardList = JSON.parse(window.localStorage.getItem("boardList")) || [];
+  
+  if (userList.length == 0 || userList.length < userId) {
+    alert("You must be logged in as a user in order to create a board.");
+    return;
+  }
+
+  const userName = userList[userId].name;
+
+  // Checking that the user doesn't already have a board with the same title.
+  for (let i = 0; i < boardList.length; i++) {
+    const boardOwnerId = boardList[i].userIds[0];
+    if (boardOwnerId == userId && boardList[i] == title) {
+      alert("Board title " + title + " already exist for user " + userName + ".");
+      return;
+    }
+  }
+  
+  // Creating the new board and adds it to the list.
+  const boardId = boardList.length + 1;
+  const userIds = [userId]; // current members (user ids) of the board
+  const columns = []; // column list
+  const board = {userIds, title, columns};
+  boardList.push(board);
+  
+  // Setting the board as the users last active board.
+  userList[userId].lastBoardId = boardId;
+
+  // Storing board and user data in local storage.
+  window.localStorage.setItem("boardList", JSON.stringify(boardList));
+  window.localStorage.setItem("userList", JSON.stringify(userList));
+  console.log("Storing board with title " + title + " (id=" + boardId + ") for user " + userName + " (id=" + userId + ").");
+  
+  // Redirect to the main page.
+  const urlParam = "?" + encodeURIComponent(boardId);
+  window.location.href = "index.html" + urlParam;
 }
--- js/global.js
+++ js/global.js
@@ -1,82 +1,11 @@
-function loginUser(event) {
-  event.preventDefault();
-
-  const loginType = document.activeElement.name;
-  //console.log(loginType + " button pressed");
-
-  const name = document.querySelector("[name='username-in']").value.toLowerCase();
-  const password = document.querySelector("[name='password-in']").value;
-
-  if (name.length < 1) {
-    console.log("Invalid username, must be at least 1 characters.");
-    return;
-  }
-  if (password.length < 1) {
-    console.log("Invalid password, must be at least 1 characters.");
-    return;
-  }
-
-  //let passMasked = '';
-  //for (let i = 0; i < password.length; i++) {passMasked += "*";}
-  //console.log("username: " + name + ", password: " + passMasked);
-
-  const userList = JSON.parse(window.localStorage.getItem("userList")) || [];
-
-  if (loginType == "login") {
-       
-     // Check if user is found in storage.
-    for (let i = 0; i < userList.length; i++) {
-      if (userList[i].name == name) {
-
-        // User found, check password.
-        if (userList[i].password == password) {
-          console.log("user authentication successful");
-          
-          // Check if user has a valid lastBoardId value set.
-          const urlParam = "?" + encodeURIComponent(userList[i].id);
-          if (userList[i].lastBoardId > 0) {
-            window.location.href = "index.html" + urlParam;
-          } else {
-            window.location.href = "boards.html" + urlParam;
-          }
-          return;
-        } else {
-          console.log("User authentication failed: invalid password");
-          // TODO: output error message in feedback div
-        }
-      
-      }
-    }
-    console.log("Couldn't find user " + name + " in storage");
-    // TODO: output error message in feedback div
-
-  } else {
-
-    // Add user to storage
-
-    // First check that the user doesn't already exists.
-    for (let i = 0; i < userList.length; i++) {
-      if (userList[i].name == name) {
-          console.log("User " + name + " already in storage");
-          // TODO: output feedback message
-          return;
-      }
-    }
-
-    const id = getUniqueListId(userList);
-    const lastBoardId = 0;
-    const user = {id, name, password, lastBoardId};
-    userList.push(user);
-    window.localStorage.setItem("userList", JSON.stringify(userList));
-
-    console.log("User " + name + " added to storage with id " + id);
-    // TODO: output feedback message
-  }
-}
-
 function getUserId() {
   const s = window.location.search;
   return s.substring(1, s.length);
+}
+
+function isAlphaNumeric(str) { 
+  var pattern = /^[0-9a-zA-Z]+$/;
+  return str.match(pattern);
 }
 
 function getUniqueListId(listName) {
@@ -84,7 +13,7 @@ function getUniqueListId(listName) {
     const list = JSON.parse(window.localStorage.getItem(listName));
     
     if (list == undefined) {
-        return 1;
+        return 0;
     }
     
     let usedIds = [];
@@ -93,7 +22,7 @@ function getUniqueListId(listName) {
         usedIds[entry.id] = true;
     }
     
-    let id = 1;
+    let id = 0;
     do {
     } while (usedIds[++id] == true);
 
