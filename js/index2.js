d4cfe44c4b2de7c23b093b2d4b6d22df9f7acf5f
olavhl <56083131+olavhl@users.noreply.github.com>
2020-05-26 15:20:07 +0200

    Fixed merge conflict

--- js/index2.js
+++ js/index2.js
@@ -239,16 +239,9 @@ function showInviteMenu() {
     memberDiv.className = "memberListName";
     memberDiv.innerHTML = userList[i].name;
     memberDiv.onclick = function() {addMemberToBoard(userId, i, boardId, memberDiv)};
-<<<<<<< HEAD
-    
-
-=======
-    memberDiv.addEventListener('keypress', function(e) {
-      if (e.keyCode == 13) {addMemberToBoard(userId, i, boardId, memberDiv);}
-    });
-    memberDiv.tabIndex = 0;
->>>>>>> 5d894a27f5e875a5267b2f1a11b494d7cb969de4
+   
     membersDiv.appendChild(memberDiv);
+    memberDiv.tabIndex = "0";
   }
 
   setTabindexOnProperties(-1);
